9a5b1d721c9161557b4fa45a6a4b091d65431ba4
ambrovko <ambrovko@mail.ru>
2022-06-05 18:58:17 +0300

    update

--- project_0/Final version.py
+++ project_0/Final version.py
@@ -1,13 +1,5 @@
 import numpy as np
 def random_predict(number:int=1)-> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
     number = np.random.randint(1,101) 
     count = 0
     guess_number = int(input("Guess the number from 1 to 100 \n"))
