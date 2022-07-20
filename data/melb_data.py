38e5ca98b20ded6ebfc8a8b42e2ea9b0ec4c87a6
ambrovko <ambrovko@mail.ru>
2022-07-20 23:15:34 +0300

    e

--- data/melb_data.py
+++ data/melb_data.py
@@ -5,3 +5,6 @@ melb_df['Date'] = pd.to_datetime(melb_df['Date'], dayfirst=True)
 display(melb_df['Date'])
 melb_df['WeekdaySale'] = melb_df['Date'].dt.dayofweek
 display(melb_df['WeekdaySale'])
+display(melb_df['WeekdaySale'].value_counts())
+weekend_count = melb_df[(melb_df['WeekdaySale'] == 5) | (melb_df['WeekdaySale'] == 6)].shape[0]
+print(weekend_count)
