a50f63fa71e97ea95acd5fc4fa2cb45be5f03cc5
ambrovko <ambrovko@mail.ru>
2022-07-20 23:01:22 +0300

    e

--- data/melb_data.py
+++ data/melb_data.py
@@ -1,3 +1,7 @@
 import pandas as pd
 from IPython.display import display
 melb_df = pd.read_csv('data/melb_data.csv', sep=',')
+melb_df['Date'] = pd.to_datetime(melb_df['Date'], dayfirst=True)
+display(melb_df['Date'])
+melb_df['WeekdaySale'] = melb_df['Date'].dt.dayofweek
+display(melb_df['WeekdaySale'])
